0cc3b6ac9f10e03446fe3d70cbf19f0ddcbabef2
Yuji YU <roujrouee@gmail.com>
2022-07-13 21:36:43 -0400

    Updated Func Name

---
+++ src/main/java/com/citizen/citizenprj/UserController.java
@@ -0,0 +1,12 @@
+package com.citizen.citizenprj;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    @GetMapping
+    public String citizenprj() {
+        return "hello world";
+    }
+}
