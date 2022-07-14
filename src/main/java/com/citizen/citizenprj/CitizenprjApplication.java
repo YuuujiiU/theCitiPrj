5c664ae3c483cd5f0590f99138ced88815477993
Yuji YU <roujrouee@gmail.com>
2022-07-13 20:52:08 -0400

    Init

---
+++ src/main/java/com/citizen/citizenprj/CitizenprjApplication.java
@@ -0,0 +1,13 @@
+package com.citizen.citizenprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitizenprjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/citizen/citizenprj/CitizenprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citizen.citizenprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
