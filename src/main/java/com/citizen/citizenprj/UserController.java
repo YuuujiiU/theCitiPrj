5eefd1a26e85de531057798bc8735bc53769dc91
Yuji YU <roujrouee@gmail.com>
2022-07-21 10:35:04 -0400

    mappingaddr

--- src/main/java/com/citizen/citizenprj/UserController.java
+++ src/main/java/com/citizen/citizenprj/UserController.java
@@ -1,11 +1,12 @@
 package com.citizen.citizenprj;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
-    @GetMapping
+    @RequestMapping("/test")
     public String citizenprj() {
         return "hello world";
     }
