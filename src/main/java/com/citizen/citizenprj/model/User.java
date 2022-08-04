4c1c35d82d42166b0450da6fd2d1f40598930258
Yuji YU <roujrouee@gmail.com>
2022-08-04 16:41:38 -0400

    database connect commit

--- src/main/java/com/citizen/citizenprj/model/User.java
+++ src/main/java/com/citizen/citizenprj/model/User.java
@@ -1,36 +1,36 @@
-package com.citizen.citizenprj.model;
+// package com.citizen.citizenprj.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+// import javax.persistence.Column;
+// import javax.persistence.Entity;
+// import javax.persistence.GeneratedValue;
+// import javax.persistence.GenerationType;
+// import javax.persistence.Id;
+// import javax.persistence.Table;
 
-import org.springframework.web.servlet.FlashMap;
+// import org.springframework.web.servlet.FlashMap;
 
-import lombok.Data;
+// import lombok.Data;
 
-@Data
-@Entity
-@Table(name = "users")
-public class User {
+// @Data
+// @Entity
+// @Table(name = "users")
+// public class User {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+// @Id
+// @GeneratedValue(strategy = GenerationType.IDENTITY)
+// private long id;
 
-    @Column(name = "name")
-    private String name;
+// @Column(name = "name")
+// private String name;
 
-    @Column(name = "email")
-    private String email;
-    private String password;
-    private String zipCode;
+// @Column(name = "email")
+// private String email;
+// private String password;
+// private String zipCode;
 
-    private int safety;
-    private int transport;
-    private int location;
-    private int noisy;
+// private int safety;
+// private int transport;
+// private int location;
+// private int noisy;
 
-}
+// }
